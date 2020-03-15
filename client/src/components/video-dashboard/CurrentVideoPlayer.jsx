a812d0ec430cfec5b2f120586a309d3f33dd6be2
SaiKumar Immadi <immadisaikumar@ymail.com>
2020-03-16 01:21:48 +0530

    current video player ui

--- client/src/components/video-dashboard/CurrentVideoPlayer.jsx
+++ client/src/components/video-dashboard/CurrentVideoPlayer.jsx
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
+import { Container } from "reactstrap";
+import { Player, BigPlayButton } from "video-react";
+import "video-react/dist/video-react.css";
 
 class CurrentVideoPlayer extends Component {
   render() {
-    return <h1>CurrentVideoPlayer</h1>;
+    const { currentVideo } = this.props;
+    console.log({ currentVideo });
+    return (
+      <Container className="mt-5">
+        <Player playsInline src={currentVideo.location}>
+          <BigPlayButton position="center" />
+        </Player>
+        <h2 className="current-video-name">{currentVideo.fileName}</h2>
+      </Container>
+    );
   }
 }
 
