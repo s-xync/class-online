458691d3e1bcee372398be37e1a849b336fad933
SaiKumar Immadi <immadisaikumar@ymail.com>
2020-03-16 01:08:32 +0530

    checks react build for the sake of heroku

---
+++ client/build/precache-manifest.3b612f83cb9ecf45be289c63a1aa9bd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39e062a4241933413276ab9d702313f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76a1afd6e2022ffd740c",
+    "url": "/static/css/main.141a6543.chunk.css"
+  },
+  {
+    "revision": "cd1645d99649addd0619",
+    "url": "/static/js/2.966a1754.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.966a1754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76a1afd6e2022ffd740c",
+    "url": "/static/js/main.39a1f663.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
