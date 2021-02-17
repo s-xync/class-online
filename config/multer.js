826166905025c5b88d64588519e585eb64d464c9
SaiKumar Immadi <immadisaikumar@ymail.com>
2021-02-17 16:39:04 +0530

    logs

--- config/multer.js
+++ config/multer.js
@@ -3,9 +3,15 @@ const multerS3 = require("multer-s3");
 const AWS = require("aws-sdk");
 const constants = require("./constants");
 
+console.log(
+  constants.AWS_ACCESS_KEY_ID,
+  constants.AWS_SECRET_ACCESS_KEY,
+  constants.AWS_S3_BUCKET_NAME
+);
+
 const s3 = new AWS.S3({
   accessKeyId: constants.AWS_ACCESS_KEY_ID,
-  secretAccessKey: constants.AWS_SECRET_ACCESS_KEY
+  secretAccessKey: constants.AWS_SECRET_ACCESS_KEY,
 });
 
 const uploadS3 = multer({
@@ -18,8 +24,8 @@ const uploadS3 = multer({
     },
     key: (req, file, cb) => {
       cb(null, Date.now().toString() + "-" + file.originalname);
-    }
-  })
+    },
+  }),
 });
 
 module.exports = { uploadS3 };
