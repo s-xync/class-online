87b5c019a361dadaabe32895176bdba362124351
SaiKumar Immadi <immadisaikumar@ymail.com>
2021-02-17 16:53:04 +0530

    removed a log

--- config/multer.js
+++ config/multer.js
@@ -3,12 +3,6 @@ const multerS3 = require("multer-s3");
 const AWS = require("aws-sdk");
 const constants = require("./constants");
 
-console.log(
-  constants.AWS_ACCESS_KEY_ID,
-  constants.AWS_SECRET_ACCESS_KEY,
-  constants.AWS_S3_BUCKET_NAME
-);
-
 const s3 = new AWS.S3({
   accessKeyId: constants.AWS_ACCESS_KEY_ID,
   secretAccessKey: constants.AWS_SECRET_ACCESS_KEY,
